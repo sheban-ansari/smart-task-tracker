a47c2c5c5a23cbac90d5fd2e8e68995ceed468c4
Sheban Ansari <shebaan242@gmail.com>
2026-09-15 23:41:36 +0530

    Configure dotenv for environment variable management
    Added dotenv configuration for environment variables.

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -8,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Connection 
+// MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/tasks")
   .then(() => console.log("MongoDB Connected"))
@@ -73,7 +74,7 @@ app.delete("/tasks/:id", async (req, res) => {
   }
 });
 
-// PORT (IMPORTANT for deploy)
+// PORT
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
